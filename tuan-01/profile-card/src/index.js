afd435954eb6911c3a47f54e93a592645adcbfe9
hoangtuanc02 <tuan4695hatinh@gmail.com>
2025-07-28 10:20:36 +0700

    full bai 1

--- tuan-01/profile-card/src/index.js
+++ tuan-01/profile-card/src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+const element =(<div className="container">
+  <div className="card">
+    <div className="card--header" />
+    <img
+      className="avatar"
+      src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      alt="avatar"
+    />
+    <div className="card--body">
+      <div>
+        <p className="text-header">Ruma Khan</p>
+        <p className="text-sub">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry
+        </p>
+        <button className="btn third">FOLLOW</button>
+      </div>
+    </div>
+  </div>
+</div>);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ element
 );
 
 // If you want to start measuring performance in your app, pass a function
